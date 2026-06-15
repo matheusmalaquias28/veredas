914383846ba17939d3be20ac6b675971a03effcd
matheusmalaquias28 <contato.matheusmalaquias@gmail.com>
2026-06-15 11:58:14 -0300

    Remove animação do cursor customizado

--- components/SiteChrome.tsx
+++ components/SiteChrome.tsx
@@ -4,7 +4,6 @@ import Navbar from '@/components/Navbar'
 import Footer from '@/components/Footer'
 import LenisSmoothScroll from '@/components/LenisSmoothScroll'
 import Preloader from '@/components/Preloader'
-import CustomCursor from '@/components/CustomCursor'
 
 export default function SiteChrome({
   children,
@@ -17,7 +16,6 @@ export default function SiteChrome({
 }) {
   return (
     <>
-      <CustomCursor />
       <LenisSmoothScroll />
       <Preloader images={preloaderImages} />
       <Navbar />
